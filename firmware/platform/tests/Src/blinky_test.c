c6b1c9171d87e92fcafa9afd714ab2ffa140a76c
Aarav Mahesh <aarav.mahesh@gmail.com>
2026-09-02 23:01:21 -0500

    disable list

--- firmware/platform/tests/Src/blinky_test.c
+++ firmware/platform/tests/Src/blinky_test.c
@@ -9,7 +9,7 @@
     #define LED_PORT GPIOB
 #elif defined(STM32G473xx)
     // LSOM
-    #define LED_PIN GPIO_PIN_5
+    #define LED_PIN GPIO_PIN_3
     #define LED_PORT GPIOC
 #else
 // Most other nucleos have a heartbeat for A5
